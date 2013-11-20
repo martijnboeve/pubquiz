0dbf0e330110c7c26919a637486d10e30672d649
Martijn Boeve <martijn.boeve@hotmail.com>
2013-11-20 23:14:43 +0100

    pretty big changes, fed team can see trello board

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
 	app = express(),
+	http = require('http'),
 	socket = require('socket.io'),
 	server = app.listen(4000),
 	io = socket.listen(server),
-	fs = require('fs'),
-	file = './test.json',
-	jQuery = require('jQuery');
+	jQuery = require('jQuery'),
+	jsonUrl = 'http://onlinepubquiz.nl/data/quizData.php';
 
 app.use('/static', express.static(__dirname + '/static'));
 
@@ -20,42 +20,69 @@ app.get('/dashboard.html', function(request, response){
 app.use(express.static(__dirname + '/Css'));
 
 var activeClients = 0,
+	currentCategory = 4,
 	currentQuestion = 0,
 	jsonQuestions = {},
 	questions = {},
 	sendQuestionNode = {},
 	teams = [];
 
-
- fs.readFile(file, 'utf8', function (err, data) {
-	if (err) {
-	console.log('Error: ' + err);
-	return;
-	}
-	jsonQuestions = JSON.parse(data);	
-}); 
+ http.get(jsonUrl, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+    res.on('end', function() {
+        jsonQuestions = JSON.parse(body)
+        console.log("Got response: ", jsonQuestions);
+    });
+}).on('error', function(e) {
+      console.log("Got error: ", e);
+});
  
 io.sockets.on('connection', function(socket){
 	
 	clientConnect(socket);
 
-	socket.on('bossSaysStartTest', function () {
+	socket.on('callStartTest', function () {
 		currentQuestion = 0;
-        io.sockets.emit('startTest');
+    currentCategory = 0;
+		var initData = {
+			quizName: ''+jsonQuestions.title,
+			quizIntro: ''+jsonQuestions.intro,
+      firstCatTitle: jsonQuestions.categories[0].title
+		};
+        io.sockets.emit('startTest', initData);
     });
 
-	socket.on('bossSaysNext', function () {
-		currentQuestion += 1;
-		sendQuestionNode = {};
-		jQuery.each(jsonQuestions.questions, function(i,elem) {
-			if (elem.questionId == currentQuestion){
-				sendQuestionNode = elem
-			}
-		});
-        io.sockets.emit('nextQuestion', sendQuestionNode);
+	socket.on('callNextQuestion', function () {
+  		sendQuestionNode = {};
+      var nextCat = false;
+      jQuery.each(jsonQuestions.categories[currentCategory].questions, function (n,question) {
+        if (n == currentQuestion) {
+          console.log('current question added');
+          sendQuestionNode = question;
+          if (n == parseInt(jsonQuestions.categories[currentCategory].questions.length - 1 )) {
+            nextCat = true;  
+          }
+        }
+      });
+        
+
+      io.sockets.emit('nextQuestion', sendQuestionNode);
+      
+
+      if (nextCat) {
+        currentCategory += 1;
+        currentQuestion = 0;
+        io.sockets.emit('nextCat', jsonQuestions.categories[currentCategory].title);
+      } else {
+        currentQuestion += 1;
+      }
+
     });
 	
-	socket.on('bossSaysStartTime', function () {
+	 socket.on('callStartTime', function () {
         io.sockets.emit('startTime', sendQuestionNode);
     });
 
@@ -65,31 +92,46 @@ io.sockets.on('connection', function(socket){
         var teamInfo = new Object();
             teamInfo.teamName     = data.teamName;
             teamInfo.clientId     = socket.id;
-            teams.push(teamInfo);
+            teamInfo.givenAnswers = [];
 
-        socket.emit('removeModal');
-        io.sockets.emit('teamsUpdate', teams);
+            var teamCheck = teams.filter(function (team) { return team.teamName.toLowerCase() == data.teamName.toLowerCase() }).length;
+            if (teamCheck < 1) {
+              teams.push(teamInfo);
+              socket.emit('removeModal');
+              io.sockets.emit('teamsUpdate', teams);
+            } else {
+              socket.emit('teamNameExists');
+            }
+            
     });
 
+    socket.on('teamSubmitAnswers', function (data) {
+      for( var i=0, len=teams.length; i<len; ++i ){
+          if(teams[i].clientId == socket.id){
+              teams[i].givenAnswers.push(data);
+          }
+      }
+      io.sockets.emit('teamsUpdate', teams);
+    });
+
+  function clientConnect(socket){
+    activeClients +=1;
+    io.sockets.emit('updateClients', {clients:activeClients});
+    socket.on('disconnect', function(){clientDisconnect()});
+  }
+   
+  function clientDisconnect(){
+    activeClients -=1;
+    io.sockets.emit('updateClients', {clients:activeClients});
+    for( var i=0, len=teams.length; i<len; ++i ){
+          var t = teams[i];
+          if(t.clientId == socket.id){
+              teams.splice(i,1);
+              break;
+          }
+      }
+      io.sockets.emit('teamsUpdate', teams);
+      console.log('client disconnected');
+  }
 
 });
- 
-function clientConnect(socket){
-  activeClients +=1;
-  io.sockets.emit('updateClients', {clients:activeClients});
-  socket.on('disconnect', function(){clientDisconnect()});
-}
- 
-function clientDisconnect(){
-  activeClients -=1;
-  io.sockets.emit('updateClients', {clients:activeClients});
-  for( var i=0, len=teams.length; i<len; ++i ){
-        var t = teams[i];
-        if(t.clientId == socket.id){
-            teams.splice(i,1);
-            break;
-        }
-    }
-    io.sockets.emit('teamsUpdate', teams);
-
-}
--- static/Scripts/main.js
+++ static/Scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$clientCounter = $("#client_count")
 
-	var full = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+	var full = location.protocol+'//'+location.hostname+':4000';
 	var socket = io.connect(full);
 	
 	socket.on('updateClients', function(msg){
@@ -12,13 +12,21 @@ $(document).ready(function () {
 		removeModal();
 	});
 
+	socket.on('teamNameExists', function () {
+		teamNameExists();
+	});
+
 	socket.on('teamsUpdate', function (data) {
 		teamsUpdate(data);
 	});	
 
 	socket.on('startTest', function (data) {
-		startTest();
+		startTest(data);
 	});	
+
+	socket.on('nextCat', function (data) {
+		nextCat(data);
+	});
 
 	socket.on('nextQuestion', function (data) {
 		nextQuestion(data);
@@ -29,75 +37,97 @@ $(document).ready(function () {
 	});
 
 	$('#startTest').on('click', function(){
-	  socket.emit('bossSaysStartTest');
+		socket.emit('callStartTest');
 	});
 	
 	$('#nextQuestion').on('click', function(){
-	  socket.emit('bossSaysNext');
+		socket.emit('callNextQuestion');
 	});
 
 	$('#startTime').on('click', function(){
-	  socket.emit('bossSaysStartTime');
+		socket.emit('callStartTime');
 	});
 
 	$('#meeDoen').on('click', function(e){
-	  e.preventDefault();
-	  if($('input[name="teamName"]').val() != '') {
-	  	var data = new Object();
-	  	data.teamName = $('input[name="teamName"]').val();
+		e.preventDefault();
+		if($('input[name="teamName"]').val() != '') {
+			var data = new Object();
+			data.teamName = $('input[name="teamName"]').val();
 
-	  	socket.emit('teamEnter', data );
-	  }
+			socket.emit('teamEnter', data );
+		}
 	});	
 
-});
 
-var updateClients = function (msg) {
-	$clientCounter.html(msg.clients);
-}
 
-var removeModal = function () {
-	$('html').removeClass('teamNameNotSet').addClass('teamNameSet');
-}
+	var updateClients = function (msg) {
+		$clientCounter.html(msg.clients);
+	}
 
-var teamsUpdate = function (data) {
-	$('#teams').empty();
-	$.each(data, function(key,val) {
-		$('#teams').append('<li>'+val.teamName+'</li>')
-	})
-}
+	var removeModal = function () {
+		$('html').removeClass('teamNameNotSet').addClass('teamNameSet');
+	}
 
-var startTest = function () {
-	$('html').addClass('testStarted');
-}
+	var teamNameExists = function () {
+		$('#enter').append('<p>Helaas, deze naam is al gekozen! Kies een andere.</p>');
+	}
 
-var nextQuestion = function (data) {
-	$('.timeDone').removeClass('timeDone');
-	$('.timer .time').css({
-		'right': '100%',
-		'-webkit-transition-duration': '300ms',
-		'-moz-transition-duration': '300ms',
-		'transition-duration': '300ms'
-	});
+	var teamsUpdate = function (data) {
+		$('#teams').empty();
+		$.each(data, function(key,val) {
+			var teamInfo = $('<div class="teamInfo clearfix"><h2>'+val.teamName+'</h2></div>');
+			$('#teams').append(teamInfo);
+			$.each(val.givenAnswers, function (i, questionInfo) {
+				if (questionInfo.question != undefined && $('html').hasClass('dash')) {
+					teamInfo.append($('<dt>'+questionInfo.question+'</dt>'));
+					teamInfo.append($('<dd>'+questionInfo.answers+'</dd>'));
+				}
+			});
+		})
+	}
 
-	if(data.questionId) {
-			$('#questionId').html(data.questionId);
+	var startTest = function (data) {
+		$('html').addClass('testStarted');
+		$('#initScreen').html(data.quizIntro +'<br /><strong>Categorie: </strong><span id="catName">' + data.firstCatTitle + '</span>');
+		$('#header h1').append('<span class="oppositeText">'+ data.quizName + '</span>');
+	}
+
+	var nextCat = function (data) {
+		$('#catName').text(data);
+	}
+
+	var nextQuestion = function (data) {
+		$('.timeDone').removeClass('timeDone');
+		$('.timer .time').css({
+			'right': '100%',
+			'-webkit-transition-duration': '300ms',
+			'-moz-transition-duration': '300ms',
+			'transition-duration': '300ms'
+		});
+		if(data.id) {
+			$('#questionId').html(data.sortOrder);
 			$('#vraag').html(data.question);
 			$('#answers').empty();
 			var $html = $('<ul />');
-			$.each(data.answers, function (index, elem) {
-				$html.append('<li><span class="chooseAnswer" data-answer="' + elem.index + '">' + elem.index + '</span> '+ elem.answer + '</li>');
-			});
-
+			for (var i = 0; i<data.numberOfAnswers; i++) {
+				var answerNumber = i+1;
+				if (i == 0) {
+					answerNumber = '';
+				}
+				$html.append('<li><label for="answer_'+i+'">Antwoord '+answerNumber+'</label><input type="text" name="answer_'+i+'" disabled /></li>');
+			}
+			
 			$('#answers').append($html);
-	} else {
+		} else {
 			endTest();
+		}
 	}
-}
 
-var startTime = function (data) {
+	var startTime = function (data) {
 
-	$('#answers li').bind('click', function (e) {
+		if (data.time == undefined) {data.time = 30}
+
+	/*$('#answers li').bind('click', function (e) {
 		$('.chooseAnswer.selected').removeClass('selected');
 		$(this).find('.chooseAnswer').addClass('selected');
 	})
@@ -105,7 +135,11 @@ var startTime = function (data) {
 	$('#answers li').bind('hover', function (e) {
 		$('.chooseAnswer.hover').removeClass('hover');
 		$(this).find('.chooseAnswer').addClass('hover');
-	})
+	})*/
+
+	$.each($('#answers li'), function() {
+		$(this).find('input[type="text"]').removeAttr('disabled') 
+	});
 
 	$('.timer .time').css({
 		'right': 0,
@@ -114,14 +148,35 @@ var startTime = function (data) {
 		'transition-duration': data.time + 's'
 	});
 
+	var itCount = 0;
+	setInterval(function () {
+		if (itCount <= parseInt(data.time)*1000) {
+			$('.timer .text').text( /* (data.time - (itCount)) + 'seconden' */);
+			itCount++;
+		}
+	}, 1000);
+
 	setTimeout( function () {
 		$('#question, #answers').addClass('timeDone');
-		$('#answers li').unbind('click');
-		$('#answers li').unbind('hover');
-	}, parseInt(data.time)*1000);
-}
+			/*$('#answers li').unbind('click');
+			$('#answers li').unbind('hover');*/
+			var givenAnswers = new Object();
+			givenAnswers.question = $('#vraag').text();
+			givenAnswers.answers = [];
+			$.each($('#answers li'), function() {
+				$(this).find('input[type="text"]').attr('disabled');
+				givenAnswers.answers.push($(this).find('input[type="text"]').val());
+			});
+			socket.emit('teamSubmitAnswers', givenAnswers );
+		}, parseInt(data.time)*1000);
+	}
 
-var endTest = function () {
-	$('#gameZone').html('THE END')
-}
+	var endTest = function () {
+		$('#gameZone').html('THE END')
+	}
 
+	window.onbeforeunload = function() {
+  		return "Are you sure you want to navigate away?";
+	}
+
+});
